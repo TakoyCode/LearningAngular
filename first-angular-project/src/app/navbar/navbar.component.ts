0a09ed80b1880d93ac83111f11a329fa2617acf8
TakoyCode <129565940+TakoyCode@users.noreply.github.com>
2024-12-11 21:51:54 +0100

    Learning more about Angular

--- first-angular-project/src/app/navbar/navbar.component.ts
+++ first-angular-project/src/app/navbar/navbar.component.ts
@@ -2,8 +2,20 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "app-navbar",
+    // this is also the standard setting for standalone, so it doesn't really need to be specified here 
     standalone: true,
-    template: '<h1>Test</h1>',
+    // template: ` 
+    // <h1>Test 1</h1>
+    // <p>Dummy text</p>
+    // `,
+    // styles: `
+    //     h1{color: purple}
+    //     p{background: gray}
+    // `,
+
+    // external html/css files have priority over inline html/css 
+    templateUrl: "./navbar.component.html",
+    styleUrl: "./navbar.component.css",
 })
 
 export class AppNavbar {
