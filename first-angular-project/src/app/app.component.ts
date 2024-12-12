f1be0642f7ed7f98fdf96cadb93a3dbb13da9a91
TakoyCode <129565940+TakoyCode@users.noreply.github.com>
2024-12-12 22:01:23 +0100

    learned about bindings in angular

--- first-angular-project/src/app/app.component.ts
+++ first-angular-project/src/app/app.component.ts
@@ -10,5 +10,11 @@ import { HeaderComponent } from './header/header.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'first-angular-project';
+  title: string = 'first-angular-project';
+  num: number = 123;
+  imgURL: string = 'https://gratisography.com/wp-content/uploads/2024/10/gratisography-happy-cone-800x525.jpg';
+  isDisabled: boolean = true;
+  isActive: boolean = true;
+  borderColor: string = "red";
+  fruitName: string = "banana"
 }
