3d6a02f90034d86e44a0c64b98b267a536f634aa
TakoyCode <129565940+TakoyCode@users.noreply.github.com>
2025-01-07 21:56:01 +0100

    Learned about Event binding and Event Filtering

--- first-angular-project/src/app/learning-css-binding/learning-css-binding.component.ts
+++ first-angular-project/src/app/learning-css-binding/learning-css-binding.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EventType } from '@angular/router';
 
 @Component({
   selector: 'app-learning-css-binding',
@@ -13,4 +14,26 @@ export class LearningCSSBindingComponent {
   isActive: boolean = true;
   borderColor: string = 'red';
   fruitName: string = 'banana';
+
+  buttonClick() {
+    console.log('Clicked');
+  }
+
+  buttonHover() {
+    console.log('Hovered');
+  }
+
+  keyPressed() {
+    console.log('Key entered');
+  }
+
+  keyEnterPressed(event: any) {
+    if (event.keyCode === 13) {
+      console.log('Enter Key Pressed');
+    }
+  }
+
+  keyupFiltering() {
+    console.log('Keyup event filtering');
+  }
 }
